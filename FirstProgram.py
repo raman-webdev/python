10d644d144bb9b28b29fa6a8597250a1d1e92800
raman-webdev <chaudharyraman562@gmail.com>
2025-09-19 15:53:10 +0545

    initial commit

---
+++ FirstProgram.py
@@ -0,0 +1,28 @@
+# print('Hello world')
+# print('My name is Raman' , 'and my age is 22.')
+# print(23)
+# print(55-12/2)
+# print(12-2)
+
+name = 'Raman'
+age = 22
+age2 = age
+print('MY name is', name, 'and age is', age2,'.')
+
+
+a = 20
+b = 10
+sum = a + b
+diff = a - b
+multi = a * b
+divide = a / b
+remain = a % b
+pow = 2 ** 3
+
+print(sum)
+print(diff)
+print(multi)
+print(divide)
+print(remain)
+print(pow)
+
